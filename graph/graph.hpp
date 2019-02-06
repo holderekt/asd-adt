04148cd569284a40a7e7f6c7b2cc0df6fe9baa71
holderekt <navispeshs@gmail.com>
2019-02-06 16:18:43 +0100

    oooh

--- graph/graph.hpp
+++ graph/graph.hpp
@@ -20,11 +20,22 @@ template <class T, class W, class L>
 class GEdge{
 public:
     friend class GNode<T,W,L>;
+    friend class Graph<T,W,L>;
+    bool isEmpty() const{ return empty; }
+    GNode<T,W,L>* getFirst(){return first;}
+    GNode<T,W,L>* getSecond(){return second;}
+    void setData(GNode<T,W,L>* first, GNode<T,W,L>* second, W weight){
+        this->first = first;
+        this->second = second;
+        this->weight = weight;
+        this->empty = false;
+    }
 
 private:
     GNode<T,W,L>* first;
     GNode<T,W,L>* second;
     W weight;
+    bool empty;
 };
 
 /*
@@ -35,28 +46,29 @@ class GNode{
 public:
     friend class Graph<T,W,L>;
     friend class GEdge<T,W,L>;
-    GNode():length(0){};
+
+    GNode():empty(true){};
+
     GNode(T value,L label):value(value),label(label),length(0){};
-
-
-    // TODO REMOVE THIS SHIT
-    void print(){
-        std::cout << "[";
-        for(int i = 0; i!=length; i++){
-            std::cout << edges[i]->first->value << " -> " << edges->second->value;
-            if(i!=length){
-                std::cout << ",";
-            }
-        }
-        std::cout << "]";
+    T getValue(){return value;}
+    size_t getId(){return id;}
+    bool isEmpty() const{return empty;};
+    void setData(T value,L label, size_t id){
+        this->value = value;
+        this->label = label;
+        this->id = id;
+        this->empty = false;
     }
 
-
+    GEdge<T,W,L>* edges;
+    
 private:
     T value;
     L label;
-    GEdge<T,W,L>* edges;
+    
     size_t length;
+    bool empty;
+    size_t id;
 };
 
 /*
@@ -65,6 +77,9 @@ private:
 template <class T, class W, class L>
 class Graph{
 public:
+    friend class GNode<T,W,L>;
+    friend class GEdge<T,W,L>;
+
     typedef T value_type;
     typedef W weight;
     typedef L label;
@@ -72,20 +87,92 @@ public:
     typedef GEdge<T,W,L> Edge; 
 
     Graph(size_t);
-    Node* insert(value_type);
+    Node* insert_node(value_type,label);
+    void insert_edge(Node*, Node*, weight);
+
+    template <class _T, class _W, class _L>
+    friend std::ostream& operator<<(std::ostream&, const Graph<_T,_W,_L>&);
+
+    Node* matrix;
+    size_t length;
+    size_t size;
 
 private:
-    Node* matrix;
-    size_t lenght;
-    size_t size;
+
 };
 
 template <class T, class W, class L>
 Graph<T,W,L>::Graph(size_t dimension){
-    lenght = 0;
+    length = 0;
     size = dimension;
     matrix = new Node[dimension];
     for(int i = 0; i!=dimension; i++){
         matrix[i].edges = new Edge[dimension];
     }
+}
+
+template <class T, class W, class L>
+typename Graph<T,W,L>::Node* Graph<T,W,L>::insert_node(value_type value, label lab){
+    if(matrix != nullptr){
+
+        int i = 0;
+        while(!matrix[i].isEmpty()){ i++; }
+
+        matrix[i].setData(value, lab, i);
+    
+        for(int j = 0; j!= size; j++){
+            matrix[i].edges[j].empty = true;
+        }
+
+        ++length;
+        return &matrix[i];
+    }
+    return nullptr;
+}
+
+/*
+TODO REMOVE THIS SHIT   
+*/
+
+template <class T, class W, class L>
+std::ostream& operator<<(std::ostream& os, const Graph<T,W,L>& gph){
+    os << "[\n";
+    for(int i =0, count = 0; i != gph.size; i++){
+        if(!(gph.matrix[i].isEmpty())){
+            os << gph.matrix[i].getValue() << ":[";
+
+            for(int j = 0; j!= gph.size; j++){
+                if(!(gph.matrix[i].edges[j].isEmpty())){
+                    os << "(";
+                    os << gph.matrix[i].edges[j].getFirst()->getValue();
+                    os << " -> ";
+                    os << gph.matrix[i].edges[j].getSecond()->getValue();
+
+                    os << ")";
+                }
+
+            }
+
+
+            os << "]";
+            count ++;
+
+            if(count < gph.length){
+                os << ",\n";
+                
+            }
+        }
+    }
+    os << "\n]";
+    return os;
+}
+
+template <class T, class W, class L>
+void Graph<T,W,L>::insert_edge(Node* n1, Node* n2, weight w){
+    if(n1->getId() < length && n2->getId() < length){
+        n1->edges[n2->getId()].setData(n1,n2, w);
+    }else{
+        throw "Index out of bound";
+    }
+
 }
--- graph/main.cpp
+++ graph/main.cpp
@@ -6,12 +6,19 @@
 using namespace std;
 
 int main(){
-    srand(time(NULL));
-    int i = rand()%40;
-    std::cout << i;
+
 
 
     Graph<int,int,std::string> grafo(5);
+
+    auto a = grafo.insert_node(3, "ciao");
+    auto b = grafo.insert_node(4,"mario");
+    auto g = grafo.insert_node(5,"cass");
+    grafo.insert_edge(a,b, 10);
+    grafo.insert_edge(a,g, 4);
+    grafo.insert_edge(b,a, 10);
+
+    cout << grafo;
 
 
     
